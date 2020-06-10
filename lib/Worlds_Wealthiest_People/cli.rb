c8c088fa0b6b51aac8b09b03ae0a0634db2e8882
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-10 23:48:29 +0000

    first commit

---
+++ lib/Worlds_Wealthiest_People/cli.rb
@@ -0,0 +1,8 @@
+require "Worlds_Wealthiest_People/cli/version"
+
+module WorldsWealthiestPeople
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/Worlds_Wealthiest_People/cli/version.rb
@@ -0,0 +1,5 @@
+module WorldsWealthiestPeople
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
