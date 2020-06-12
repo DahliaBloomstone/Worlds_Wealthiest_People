2e11cb5d83c9dfd507c1971f685edc753504143d
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-12 00:28:36 +0000

    modified folders

--- lib/Worlds_Wealthiest_People/cli.rb
+++ lib/Worlds_Wealthiest_People/cli.rb
@@ -8,7 +8,8 @@ class WorldsWealthiestPeople::CLI
     goodbye 
 end
 
-def list_billionaires 
+def list_billionaires
+puts ""
 puts "2019 Billionaire List in order from least to greatest:"
 @people = WorldsWealthiestPeople::Person.list
 @people.each.with_index(1) do |person, i| 
@@ -19,6 +20,7 @@ end
 def menu
    input = nil
      while input != "exit"
+  puts ""
   puts "Continue to see the list of billionaires for 2019 with ranking, name, and description or type exit to enter or type list to see the full list of billionaires again or type exit:"
   input = gets.strip.downcase
  
@@ -28,12 +30,14 @@ def menu
     elsif input == "list"
     list_billionaires 
      else 
+       puts ""
         puts "Not sure what you want, type list or exit"
       end 
     end
   end 
 
 def goodbye 
+  puts ""
   puts "See you tomorrow to keep reading!"
   end 
 end 
