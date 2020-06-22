c503cb3225ef544011d8111e90852d0fcc687fe8
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-22 18:39:24 +0000

    modified folders

--- lib/Worlds_Wealthiest_People/person.rb
+++ lib/Worlds_Wealthiest_People/person.rb
@@ -1,33 +1,32 @@
 #To Interact: ./bin/Worlds-Wealthiest-People
 
-class WorldsWealthiestPeople::Person 
+class WorldsWealthiestPeople::Person
  attr_accessor :name_and_ranking, :description
- 
+
 # @@all = []
- 
- #def self.list 
-#  Scrape billionaire website and then return people based on that data 
- # self.scrape_people
-# end 
-      
-def self.scrape_people  
-  people = [] 
-  
+
+def self.list
+#  Scrape billionaire website and then return people based on that data
+  self.scrape_people
+ end 
+
+def self.scrape_people
+  people = []
+
   people << self.scrape_url
     people
-  end 
-  
-  def self.scrape_url 
-    doc = Nokogiri::HTML(open("https://vocal.media/geeks/top-50-billionaires-in-the-world-for-2019"))
-   
-   person = self.new 
-   person.name_and_ranking = doc.search("h3.css-rv324f-Heading.em4arlq0").text.strip
-   person.description = doc.search("p.css-1oblyum-P.e1ccqnho0").text.strip 
-   person
-  end 
-end 
-  
-# move the scraper into its own class
-# return values / iterators 
-# live coding exercise 
+  end
 
+  def self.scrape_url
+    doc = Nokogiri::HTML(open("https://vocal.media/geeks/top-50-billionaires-in-the-world-for-2019"))
+
+   person = self.new
+   person.name_and_ranking = doc.search("h3.css-rv324f-Heading.em4arlq0").text.strip
+   person.description = doc.search("p.css-1oblyum-P.e1ccqnho0").text.strip
+   person
+  end
+end
+
+# move the scraper into its own class
+# return values / iterators
+# live coding exercise
---
+++ lib/Worlds_Wealthiest_People/scraper.rb
@@ -0,0 +1,13 @@
+# to interact: ./bin/Worlds-Wealthiest-People
+
+
+# Class WorldsWealthiestPeople::Scraper
+# def self.scrape_url
+#  doc = Nokogiri::HTML(open("https://vocal.media/geeks/top-50-billionaires-in-the-world-for-2019"))
+
+ # person = self.new
+ # person.name_and_ranking = doc.search("h3.css-rv324f-Heading.em4arlq0").text.strip
+#  person.description = doc.search("p.css-1oblyum-P.e1ccqnho0").text.strip
+#  person
+# end
+# end
